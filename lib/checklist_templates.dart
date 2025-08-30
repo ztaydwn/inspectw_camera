61358e4fa65dc4cdc5124f5d4db0a5a5816c60ef
diego mancilla <92diegomancilla@gmail.com>
2025-08-30 16:11:48 -0500

    CHECKLIST CORRECTA

--- lib/checklist_templates.dart
+++ lib/checklist_templates.dart
@@ -4,30 +4,80 @@ const List<ChecklistTemplate> kChecklistTemplates = [
   ChecklistTemplate(
     name: 'Tablero Eléctrico',
     items: [
-      ChecklistItemTemplate(title: 'Vista panorámica del tablero'),
-      ChecklistItemTemplate(title: 'Identificación y señalización del tablero'),
-      ChecklistItemTemplate(title: 'Interruptor principal'),
-      ChecklistItemTemplate(title: 'Interruptores secundarios'),
-      ChecklistItemTemplate(title: 'Barras de distribución'),
-      ChecklistItemTemplate(title: 'Conexiones y cableado interno'),
-      ChecklistItemTemplate(title: 'Puesta a tierra'),
-      ChecklistItemTemplate(title: 'Estado de la puerta y cerrojo'),
-      ChecklistItemTemplate(title: 'Espacio libre de trabajo frontal'),
-      ChecklistItemTemplate(title: 'Observaciones adicionales'),
+      ChecklistItemTemplate(
+          title:
+              '1.1.3	Cuenta con señalizacion de RIESGO ELECTRICO fotoluminiscente?'),
+      ChecklistItemTemplate(
+          title:
+              '1.2.1	El gabinete del tablero se encuentra en buen estado de conservación?'),
+      ChecklistItemTemplate(
+          title:
+              '1.2.2	Cuenta con ITM que corresponden a la capacidad de corriente de los conductores eléctricos que protege?'),
+      ChecklistItemTemplate(
+          title:
+              '1.2.3	Cuenta con un circuito eléctrico por cada interruptor termomagnético?'),
+      ChecklistItemTemplate(
+          title:
+              '1.2.3	El tablero tiene un interruptor general en su interior o adyacente al mismo?'),
+      ChecklistItemTemplate(
+          title:
+              '1.2.6	Los ITM, DIF u otros accesorios cuenta con terminales?'),
+      ChecklistItemTemplate(
+          title:
+              '1.2.6	Está libre de empalmes no técnicos dentro del tablero?'),
+      ChecklistItemTemplate(
+          title:
+              '1.2.6	Las fases de los circuitos están diferenciados por colores o rotulados?'),
+      ChecklistItemTemplate(
+          title:
+              '1.2.6	Los cables son del tipo no propagador del incendio, libre de halógenos?'),
+      ChecklistItemTemplate(
+          title:
+              '3.1.1	El tablero eléctrico de material metálico está conectado a tierra?'),
+      ChecklistItemTemplate(
+          title: '3.1.1	Todos los circuitos tienen alimentadores de tierra?'),
+      ChecklistItemTemplate(
+          title: '3.1.2	El tablero cuenta con placa de protección (mandil)?'),
+      ChecklistItemTemplate(
+          title:
+              '3.1.3	Las aberturas no usadas (reserva) se encuentran cerradas con tapas?'),
+      ChecklistItemTemplate(
+          title:
+              '3.1.4	Todos los circuitos eléctricos tienen protección de interruptores diferenciales?'),
+      ChecklistItemTemplate(
+          title:
+              '3.1.17	Los diagramas unifilares concuerdan con lo verificado físicamente?'),
+      ChecklistItemTemplate(
+          title:
+              '3.3.7	El(los) tablero(s) eléctrico(s) cuenta(n) con identificación?'),
+      ChecklistItemTemplate(
+          title:
+              '3.3.8	Tiene directorio de los circuitos, indicando de manera visible y clara la instalación que controla?'),
+      ChecklistItemTemplate(
+          title: '3.3.8	Los circuitos se encuentran debidamente rotulados?'),
+      ChecklistItemTemplate(
+          title:
+              '3.3.9	Existe espacio libre no menor a un metro frente a los tableros eléctricos?'),
+      ChecklistItemTemplate(
+          title:
+              '3.3.10	Existe iluminación general y de emergencia en la zona de ubicación de los tableros eléctricos?'),
+      ChecklistItemTemplate(title: 'S/N	Otras observaciones'),
     ],
   ),
   ChecklistTemplate(
     name: 'Inspección de Extintor',
     items: [
-      ChecklistItemTemplate(title: 'Ubicación y visibilidad'),
-      ChecklistItemTemplate(title: 'Señalización adecuada'),
-      ChecklistItemTemplate(title: 'Manómetro de presión (si aplica)'),
-      ChecklistItemTemplate(title: 'Sello de seguridad intacto'),
-      ChecklistItemTemplate(title: 'Manguera y boquilla'),
-      ChecklistItemTemplate(title: 'Estado del cilindro (sin corrosión o daños)'),
-      ChecklistItemTemplate(title: 'Fecha de último mantenimiento/recarga'),
-      ChecklistItemTemplate(title: 'Tarjeta de inspección'),
-      ChecklistItemTemplate(title: 'Observaciones adicionales'),
+      ChecklistItemTemplate(title: '1.3.2	Está en un lugar accesible?'),
+      ChecklistItemTemplate(title: '1.3.2	Está colgado o en pedestal?'),
+      ChecklistItemTemplate(title: '1.3.2	Altura menor a 1.50m?'),
+      ChecklistItemTemplate(title: '1.3.2	Está señalizado foto. según tipo?'),
+      ChecklistItemTemplate(title: '1.3.2	N° en la señalet.?'),
+      ChecklistItemTemplate(title: '1.3.2	N° en el extintor?'),
+      ChecklistItemTemplate(title: '1.3.2	Tiene tarjeta de control?'),
+      ChecklistItemTemplate(title: '1.3.2	Está correctamente llenada?'),
+      ChecklistItemTemplate(title: '1.3.2	Tiene precinto de seguridad?'),
+      ChecklistItemTemplate(title: '1.3.3	Interperie - en gabinete?'),
+      ChecklistItemTemplate(title: '1.3.3	Estado del gabinete'),
     ],
   ),
 ];
